3c06cb172e63f2dfe445901df59bfae5f83201b0
Torcher01 <miskichekov@gmail.com>
2024-06-16 12:29:51 +0700

    Доделывает бургер-меню

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,15 +1,14 @@
 const burgerMenuButton = document.querySelector('.hero__header-burger-menu');
 const burgerMenuIconWrapper = document.querySelector('.hero__header-burger-menu-icon-wrapper');
 const burgerMenuText = document.querySelector('.hero__header-burger-menu-text');
-const burgerMenuIcon = document.querySelector('.hero__header-burger-menu-icon');
 const burgerMenuIconOpen = document.querySelector('.hero__header-burger-menu-icon--open');
 const burgerMenuIconClose = document.querySelector('.hero__header-burger-menu-icon--close');
 
 const headerMenu = document.querySelector('.hero__header-menu');
-const menuList = document.querySelector('.hero__header-menu-list');
 const menuInnerLists = document.querySelectorAll('.hero__header-menu-list--inner');
 const menuItems = document.querySelectorAll('.hero__header-menu-item--expand');
 const menuInnerListsArrows = document.querySelectorAll('.hero__header-menu-list-inner-arrow');
+const menuItemLinks = document.querySelectorAll('.hero__header-menu-item-link');
 
 const burgerMenuOpen = () => {
   burgerMenuButton.classList.add('hero__header-burger-menu--opened');
@@ -37,20 +36,10 @@ const burgerMenuClose = () => {
 }
 
 burgerMenuButton.addEventListener('click', burgerMenuOpen);
-window.addEventListener('click', (evt) => {
+document.body.addEventListener('click', (evt) => {
   const target = evt.target;
-  if (target !== burgerMenuButton
-    && target !== menuList
-    && target !== headerMenu
-    && target !== burgerMenuText
-    && target !== burgerMenuIconWrapper
-    && target !== burgerMenuIcon
-    && target !== document.querySelector('.hero__header-menu-item--programs')
-    && target !== document.querySelector('.hero__header-menu-item--news')
-    && target !== document.querySelector('.hero__header-menu-item--project')
-    && target !== document.querySelector('.hero__header-menu-item--route')
-    && target !== document.querySelector('.hero__header-menu-item--reviews')
-    && target !== document.querySelector('.hero__header-menu-item--contacts')
+  if (!target.closest('.hero__header-burger-menu')
+    && !target.closest('.hero__header-menu')
   ) {
     burgerMenuClose()
   }
@@ -58,8 +47,11 @@ window.addEventListener('click', (evt) => {
 
 menuItems.forEach((item, i) => {
   item.addEventListener('click', (evt) => {
-    evt.preventDefault();
     menuInnerLists[i].classList.toggle('hero__header-menu-list--hidden');
     menuInnerListsArrows[i].classList.toggle('hero__header-menu-list-inner-arrow--opened')
   })
+})
+
+menuItemLinks.forEach((link) => {
+  link.addEventListener('click', burgerMenuClose);
 })
